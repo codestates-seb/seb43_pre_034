ed661f644110980061f4a2864ab03fbd108050a1
chochojj <csgy114@gmail.com>
2023-04-19 01:59:53 +0900

    fix: 브랜치 실수 수정

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,7 +4,9 @@ import { LoginBtn } from "../components/common/Buttons";
 import { FcGoogle } from "react-icons/fc";
 import { AiFillGithub } from "react-icons/ai";
 import { FaFacebookSquare } from "react-icons/fa";
+import { BiLinkExternal } from "react-icons/bi";
 import logo from "../assets/images/stack.png";
+import { Link } from "react-router-dom";
 
 //전체 로그인 박스
 const LoginSection = styled.section`
@@ -17,7 +19,7 @@ const LoginSection = styled.section`
   .logo {
     width: 48px;
     height: 37px;
-    margin-bottom: 30px;
+    margin-bottom: 25px;
   }
 `;
 
@@ -70,11 +72,10 @@ const FacebookButton = styled(LoginBtn)`
 //이메일 로그인 박스
 const LoginBox = styled.div`
   width: 278px;
-  height: 230px;
   box-shadow: 0px 10px 25px 0px rgba(0, 0, 0, 0.2);
   border-radius: 10px;
   margin-top: 10px;
-  background-color: aliceblue;
+  background-color: white;
   @media ${({ theme }) => theme.breakpoints.mobileMax} {
     width: 267px;
   }
@@ -84,19 +85,115 @@ const LoginBox = styled.div`
 const LoginForm = styled.form`
   display: flex;
   flex-direction: column;
-  width: 84%;
-  margin: 0 8%;
+  width: 82%;
+  margin: 10px 9% 12px;
+  button {
+    height: 36px;
+    margin-top: 8px;
+    margin-bottom: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgb(10, 149, 255);
+    color: white;
+    border: none;
+    border-radius: 3px;
+    box-shadow: 0px 1px 0px 0px inset rgba(255, 255, 255, 0.5);
+  }
+  button:hover {
+    background: hsl(206, 100%, 40%);
+  }
+  button:active {
+    background: hsl(209, 100%, 37.5%);
+  }
+`;
+const LoginTxt = styled.div`
+  margin-top: 13px;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  label {
+    font-size: 15px;
+  }
+`;
+const LinkPwd = styled(Link)`
+  font-size: 12px;
+  color: rgb(0, 116, 204);
+  text-decoration: none;
 `;
 
-//로그인 인풋
-const LoginInput = styled.input``;
+const LoginInput = styled.input`
+  margin: 5px 0px;
+  height: 28px;
+  border-radius: 3px;
+  border: 1px solid rgb(186, 191, 196);
+  &:focus {
+    outline: none;
+    border-color: rgb(10, 149, 255);
+  }
+  &.ErrorInput {
+    border-color: rgb(208, 57, 62);
+  }
+`;
+const LinkBox = styled.div`
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  font-size: 13px;
+  span {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  span:first-child {
+    margin-bottom: 15px;
+  }
+`;
+const SignLink = styled(Link)`
+  text-decoration: none;
+  color: rgb(0, 116, 204);
+  margin-left: 3px;
+  div {
+    display: flex;
+  }
+`;
+
+const ErrorMsg = styled.span`
+  font-size: 11px;
+  color: rgb(208, 57, 62);
+`;
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [emailInputClass, setEmailInputClass] = useState("");
+  const [passwordInputClass, setPasswordInputClass] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // 이메일 양식 검증
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setEmailError("The email is not a valid email address.");
+      setEmailInputClass("ErrorInput");
+      return;
+    } else {
+      setEmailError("");
+      setEmailInputClass("");
+    }
+
+    // 비밀번호 길이 검증
+    if (password === "") {
+      setPasswordError("Password is required.");
+      setPasswordInputClass("ErrorInput");
+    } else if (password.length < 8) {
+      setPasswordError("Password must be at least 8 characters long");
+      setPasswordInputClass("ErrorInput");
+    } else {
+      setPasswordError("");
+    }
   };
 
   return (
@@ -116,32 +213,46 @@ const Login = () => {
       </FacebookButton>
       <LoginBox>
         <LoginForm onSubmit={handleSubmit}>
-          <span>Email</span>
+          <LoginTxt>
+            <label htmlFor="email">Email</label>
+          </LoginTxt>
           <LoginInput
             type="text"
             placeholder=""
             value={email}
+            className={emailInputClass}
             onChange={(event) => setEmail(event.target.value)}
           />
-          <div>
-            <span>Password</span>
-            <span>Forgot password?</span>
-          </div>
+          {emailError && <ErrorMsg>{emailError}</ErrorMsg>}
+          <LoginTxt>
+            <label htmlFor="password">Password</label>
+            <LinkPwd to="/users/login">Forgot password?</LinkPwd>
+          </LoginTxt>
           <LoginInput
             type="password"
             placeholder=""
             value={password}
+            className={passwordInputClass}
             onChange={(event) => setPassword(event.target.value)}
           />
+          {passwordError && <ErrorMsg>{passwordError}</ErrorMsg>}
           <button type="submit">Log in</button>
         </LoginForm>
       </LoginBox>
-      <span>
-        Don’t have an account? <span>Sign up</span>
-      </span>
-      <span>
-        Are you an employer? <span>Sign up on Talent</span>
-      </span>
+      <LinkBox>
+        <span>
+          Don’t have an account? <SignLink to="/users/">Sign up</SignLink>
+        </span>
+        <span>
+          Are you an employer?{" "}
+          <SignLink to="/users/login">
+            <div>
+              Sign up on Talent
+              <BiLinkExternal size="15" />
+            </div>
+          </SignLink>
+        </span>
+      </LinkBox>
     </LoginSection>
   );
 };
