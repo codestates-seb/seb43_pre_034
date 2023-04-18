c53859dcc3acba290e24ca131ac3e3ae5418af4b
kijiwon <kiji1313@gmail.com>
2023-04-18 14:09:59 +0900

    feat: Sidebar component 구현 시작

---
+++ client/src/components/common/Sidebar.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+// sidebar 공통 스타일
+const SideBarCompo = styled.div``;
+
+// 메인페이지 + 상세페이지 사이드바
+const SideBar = () => {
+  return <></>;
+};
+
+// 수정페이지 사이드바
+const SideBarEdit = () => {
+  return <></>;
+};
+export { SideBar, SideBarEdit };
