c26e505644ae2c14df00c9146e66e8215424c658
chochojj <csgy114@gmail.com>
2023-04-18 14:50:42 +0900

    feat: 로그인 버튼 구현

--- client/src/components/common/Buttons.jsx
+++ client/src/components/common/Buttons.jsx
@@ -28,4 +28,23 @@ const PostAnswerBtn = () => {
   );
 };
 
+const ButtonLogin = styled.button`
+  height: 38px;
+  background-color: #0a95ff;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  font-size: 14px;
+  padding: 10px;
+  white-space: nowrap;
+  cursor: pointer;
+  display: inline-block;
+`;
+
+const LoginBtn = ({children,...rest}) => {
+  return (
+    <LoginBtn {...rest}>{children}</LoginBtn>
+  );
+};
+
 export { AskQuestionBtn, PostAnswerBtn };
