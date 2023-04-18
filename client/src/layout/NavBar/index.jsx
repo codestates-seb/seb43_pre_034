0108fbc548957c042c7beeecddc4d7d718f5ddfe
chochojj <csgy114@gmail.com>
2023-04-18 15:33:28 +0900

    fix: NavSvg 경로 수정

--- client/src/layout/NavBar/index.jsx
+++ client/src/layout/NavBar/index.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { GlobeSVG } from "../../assets/NavSvg";
+import { GlobeSVG } from "../../assets/NavSvg";
 
 const NavBarContainer = styled.nav`
   display: flex;
