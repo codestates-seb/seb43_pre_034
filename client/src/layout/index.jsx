50312973e62da95bbe39d8a80693f1741b3efe20
sineTlsl <limsieun0313@gmail.com>
2023-04-18 14:04:04 +0900

    design: NavBar + Main 부모 컴포넌트 추가

--- client/src/layout/index.jsx
+++ client/src/layout/index.jsx
@@ -1,15 +1,24 @@
+import styled from "styled-components";
+
 // components
 import Header from "./Header";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import Main from "./Main";
 
+const Container = styled.div`
+  display: flex;
+  width: ${(props) => props.theme.widthSize.contentMax};
+`;
+
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <NavBar />
-      <Main>{children}</Main>
+      <Container>
+        <NavBar />
+        <Main>{children}</Main>
+      </Container>
       <Footer />
     </>
   );
