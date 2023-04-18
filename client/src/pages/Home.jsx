a29bcde75b4dafd93770f1f1cec424189c786f74
sineTlsl <limsieun0313@gmail.com>
2023-04-18 17:53:42 +0900

    design: 전체적인 레이아웃 수정

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,15 @@
-const Home = () => {};
+import styled from "styled-components";
+
+const Container = styled.div`
+  flex: 0 1 calc(1264px - 164px);
+  display: flex;
+  height: 200px;
+  background: black;
+  border: 2px solid yellow;
+`;
+
+const Home = () => {
+  return <Container></Container>;
+};
 
 export default Home;
