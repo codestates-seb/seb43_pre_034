c9b5d7a8caa407823cf430ba4b64ee1f984158a7
CordJG <wjrmffldglem@gmail.com>
2023-04-19 01:21:34 +0900

    Feat : entity,dto,repo,auditable 기능 구현

---
+++ server/src/main/java/com/codestates/stackoverflow/audit/Auditable.java
@@ -0,0 +1,30 @@
+package com.codestates.stackoverflow.audit;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+
+    @CreatedDate
+    @Column(name = "CREATED_AT", updatable = false)
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    @Column(name = "LAST_MODIFIED_AT")
+    private LocalDateTime modifiedAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+}
