0bce0ca290b297e31828b904de6c3402240c78ae
uknow <yunhoknight@naver.com>
2023-04-19 16:13:59 +0900

    Feat: User Repository 및 Service 구현 UserRepository와 UserService를 구현했습니다. UserService - createUser - findUser - findUsers - updateUser - deleteUser Issue#52

---
+++ server/src/main/java/com/codestates/stackoverflow/user/service/UserService.java
@@ -0,0 +1,71 @@
+package com.codestates.stackoverflow.user.service;
+
+import com.codestates.stackoverflow.exception.BusinessLogicException;
+import com.codestates.stackoverflow.exception.ExceptionCode;
+import com.codestates.stackoverflow.user.entity.User;
+import com.codestates.stackoverflow.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+
+    public User createUser(User user) {
+        verifyExistEmail(user.getEmail());
+        return userRepository.save(user);
+    }
+
+    public User findUser(long userId) {
+        return findVerifiedUser(userId);
+    }
+
+
+    public Page<User> findUsers(int page, int size) {
+        return userRepository.findAll(PageRequest.of(page, size,
+                Sort.by("userId").descending()));
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
+    public User updateUser(long userId, User user) {
+        User findUser = findVerifiedUser(user.getUserId());
+
+        Optional.ofNullable(user.getName()).ifPresent(name -> findUser.setName(name));
+        Optional.ofNullable(user.getStatus()).ifPresent(status -> findUser.setStatus(status));
+
+        return userRepository.save(findUser);
+    }
+
+    public void deleteUser(long userId) {
+        User findUser = findVerifiedUser(userId);
+
+        userRepository.delete(findUser);
+    }
+
+    @Transactional(readOnly = true)
+    public User findVerifiedUser(long userId) {
+        Optional<User> otionalUser = userRepository.findById(userId);
+        User findUser = otionalUser.orElseThrow(() ->
+                new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
+        return findUser;
+    }
+
+    private void verifyExistEmail(String email) {
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isPresent())
+            throw new BusinessLogicException(ExceptionCode.USER_EXISTS);
+    }
+}
