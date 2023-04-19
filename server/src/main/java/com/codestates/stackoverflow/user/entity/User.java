7933700e61e09b79347f04e34c52cec87a013d4c
uknow <yunhoknight@naver.com>
2023-04-19 13:13:18 +0900

    feat: User Entity 설계 및 구현 Uset Entity 구현 했습니다. issue #17

---
+++ server/src/main/java/com/codestates/stackoverflow/user/entity/User.java
@@ -0,0 +1,60 @@
+package com.codestates.stackoverflow.user.entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column(nullable = false, updatable = false, unique = true)
+    private String email;
+    @Getter(AccessLevel.NONE)
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private Status status = Status.MEMBER_ACTIVE;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>(); // <ROLE_USER
+
+    private int score;
+
+    // User별 Question과 Answer의 Score 합산
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public enum Status {
+        MEMBER_ACTIVE("활동중"),
+        MEMBER_SLEEP("휴면 상태"),
+        MEMBER_QUIT("탈퇴 상태");
+        @Getter
+        private String status;
+        Status(String status) {
+            this.status = status;
+        }
+    }
+}
