d5d4d4852bd858138135affb1826fd52ed653189
sineTlsl <limsieun0313@gmail.com>
2023-04-20 12:40:54 +0900

    design: 질문 작성 페이지 레이아웃 구성
    feat: ReactQuill 웹 에디터 추가
    
    feat: 공통적으로 사용하는 파란색 배경 버튼 컴포넌트 추가
    
    feat: Ask 질문 작성 Tip 컴포넌트 추가
    
    feat: Ask 질문 작성 Title 컴포넌트 추가
    
    feat: Ask 질문 작성 Guide 추가
    
    design: 질문 작성 페이지 레이아웃 구성

--- client/src/components/common/Buttons.jsx
+++ client/src/components/common/Buttons.jsx
@@ -41,4 +41,40 @@ const LoginBtn = ({ children, ...rest }) => {
   return <ButtonLogin {...rest}>{children}</ButtonLogin>;
 };
 
-export { AskQuestionBtn, PostAnswerBtn, LoginBtn };
+const GlobalBtn = styled.button`
+  background: #0a95ff;
+  color: #fff;
+  border: solid 1px #7aa7c7;
+  border-radius: 3px;
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: ${({ padding }) => padding};
+  cursor: pointer;
+  font-size: 13px;
+`;
+
+/**
+ * 공통적으로 사용하는 파란색 배경의 버튼 컴포넌트
+ * @property {string} height 높이
+ * @property {string} width 너비
+ * @property {string} padding 패딩
+ * @property {string} buttonText 텍스트
+ */
+
+const BluebgBtn = ({ height, width, padding, buttonText }) => {
+  return (
+    <GlobalBtn
+      width={width || "100%"}
+      height={height || "38px"}
+      padding={padding || "10.4px"}
+      // onClick={onClick}
+    >
+      {buttonText}
+    </GlobalBtn>
+  );
+};
+
+export { AskQuestionBtn, PostAnswerBtn, LoginBtn, BluebgBtn };
