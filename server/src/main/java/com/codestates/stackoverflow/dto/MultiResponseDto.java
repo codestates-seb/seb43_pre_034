d44d8da50cfa501b80dd9d50a5f4d2878ad27d4a
YuJeSeon <dbwptjs625@gmail.com>
2023-04-20 19:39:52 +0900

    be-fix-pageinfo#98

--- server/src/main/java/com/codestates/stackoverflow/dto/MultiResponseDto.java
+++ server/src/main/java/com/codestates/stackoverflow/dto/MultiResponseDto.java
@@ -5,16 +5,14 @@ import org.springframework.data.domain.Page;
 
 import java.util.List;
 
-
 @Getter
 public class MultiResponseDto<T> {
     private List<T> data;
-    private PageDto pageInfo;
+    private PageInfo pageInfo;
 
     public MultiResponseDto(List<T> data, Page page) {
         this.data = data;
-
-        this.pageInfo = new PageDto(data,page);
-
+        this.pageInfo = new PageInfo(page.getNumber() + 1,
+                page.getSize(), page.getTotalElements(), page.getTotalPages());
     }
 }
--- server/src/main/java/com/codestates/stackoverflow/question/controller/QuestionController.java
+++ server/src/main/java/com/codestates/stackoverflow/question/controller/QuestionController.java
@@ -1,7 +1,6 @@
 package com.codestates.stackoverflow.question.controller;
 
 import com.codestates.stackoverflow.dto.MultiResponseDto;
-import com.codestates.stackoverflow.dto.PageDto;
 import com.codestates.stackoverflow.dto.SingleResponseDto;
 import com.codestates.stackoverflow.question.dto.QuestionDto;
 import com.codestates.stackoverflow.question.dto.QuestionResponseDto;
@@ -11,6 +10,7 @@ import com.codestates.stackoverflow.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -55,7 +55,7 @@ public class QuestionController {
     }
 
     @GetMapping
-    public ResponseEntity getQuestions(Pageable pageable) {
+    public ResponseEntity getQuestions(@PageableDefault(page = 1, size = 15)Pageable pageable) {
         Page<Question> questionPage = questionService.getQuestions(pageable);
         List<Question> questionList = questionPage.getContent();
         return new ResponseEntity<>(
