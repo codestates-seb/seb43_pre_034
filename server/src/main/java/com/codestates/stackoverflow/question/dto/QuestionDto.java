13bc991dbbff9413253f9d76904c918c8492df54
YuJeSeon <dbwptjs625@gmail.com>
2023-04-21 10:43:42 +0900

    be-fix-patch2#104

--- server/src/main/java/com/codestates/stackoverflow/question/dto/QuestionDto.java
+++ server/src/main/java/com/codestates/stackoverflow/question/dto/QuestionDto.java
@@ -1,6 +1,8 @@
 package com.codestates.stackoverflow.question.dto;
 
 import lombok.*;
+
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 
 public class QuestionDto {
@@ -8,9 +10,9 @@ public class QuestionDto {
     @Getter
     @NoArgsConstructor
     public static class QuestionPostDto {
-        @NotEmpty(message = "질문 제목을 작성해야 합니다.")
+        @NotBlank(message = "질문 제목을 작성해야 합니다.")
         private String title;
-        @NotEmpty(message = "질문 내용을 작성해야 합니다.")
+        @NotBlank(message = "질문 내용을 작성해야 합니다.")
         private String body;
         private long userId;
         private long questionId;
@@ -20,9 +22,9 @@ public class QuestionDto {
     @Setter
     @Getter
     public static class QuestionPatchDto {
-        @NotEmpty(message = "질문 제목을 작성해야 합니다.")
+        @NotBlank(message = "질문 제목을 작성해야 합니다.")
         private String title;
-        @NotEmpty(message = "질문 내용을 작성해야 합니다.")
+        @NotBlank(message = "질문 내용을 작성해야 합니다.")
         private String body;
         private long userId;
         private long questionId;
