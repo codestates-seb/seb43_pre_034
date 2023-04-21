ba8a29e741c278148303b9e5f55f2f90198af0d2
sineTlsl <limsieun0313@gmail.com>
2023-04-21 16:17:06 +0900

    fix: 웹 에디터 Custom

--- client/src/components/QuillEditor.jsx
+++ client/src/components/QuillEditor.jsx
@@ -1,19 +1,60 @@
-import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
-const QuillEditor = ({ EditorHeight, EditorWidth }) => {
-  const [value, setValue] = useState("");
+const QuillEditor = ({
+  editorHeight,
+  editorWidth,
+  setEditorText,
+  editorText,
+  inputValue,
+}) => {
+  const modules = {
+    toolbar: {
+      container: [
+        ["bold", "italic", "underline", "strike", "blockquote", "code-block"],
+        [{ list: "ordered" }, { list: "bullet" }],
+        [
+          { align: "" },
+          { align: "center" },
+          { align: "right" },
+          { align: "justify" },
+        ],
+        ["clean"],
+      ],
+    },
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "code-block",
+    "list",
+    "bullet",
+    "align",
+    "color",
+    "background",
+  ];
+
+  const onChangeHandler = (content) => {
+    setEditorText({ ...editorText, content: content });
+  };
+
   return (
-    <>
+    <div name="content">
       <ReactQuill
-        style={{ height: EditorHeight, width: EditorWidth }}
+        style={{ height: editorHeight, width: editorWidth }}
+        modules={modules}
+        formats={formats}
         theme="snow"
-        value={value}
-        onChange={setValue}
+        value={inputValue}
+        onChange={(e) => onChangeHandler(e)}
         placeholder="내용을 입력하세요."
       />
-    </>
+    </div>
   );
 };
 
