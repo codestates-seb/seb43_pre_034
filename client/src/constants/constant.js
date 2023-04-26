220691f527aa962f9689c9db7bffc8b673a8aecf
rickyshu <hoinleekk@naver.com>
2023-04-26 23:37:12 +0900

    design:마이 페이지 수정 변경

--- client/src/constants/constant.js
+++ client/src/constants/constant.js
@@ -1,6 +1,9 @@
+//userId 정보 가져오기
+const userId = localStorage.getItem("userId");
+
 const MY_PAGE_URL_PATH = {
-  USERS_PROFILE: "/users/info",
-  USERS_PROFILE_EDIT: "/users/info/edit",
+  USERS_PROFILE: `/users/${userId}/info`,
+  USERS_PROFILE_EDIT: `/users/${userId}/info/edit`,
 };
 
 export { MY_PAGE_URL_PATH };
