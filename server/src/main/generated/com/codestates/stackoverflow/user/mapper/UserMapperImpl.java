419781de508a587e7a7cc57d2db199b67a80a760
uknow <yunhoknight@naver.com>
2023-04-26 11:38:45 +0900

    Refector: Security Cors 수정 Security Cors 수정 Cors관련하여 허용 메서드를 모두로 변경했습니다. Issue #167

--- server/src/main/generated/com/codestates/stackoverflow/user/mapper/UserMapperImpl.java
+++ server/src/main/generated/com/codestates/stackoverflow/user/mapper/UserMapperImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-04-26T10:46:09+0900",
+    date = "2023-04-26T11:25:37+0900",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.17 (Azul Systems, Inc.)"
 )
 @Component
