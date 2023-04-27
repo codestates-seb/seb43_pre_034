c8357c2927e78de24517a29a1edb6555e94af3c7
YuJeSeon <dbwptjs625@gmail.com>
2023-04-27 14:20:03 +0900

    be-refector-questionRefector#200

--- server/src/main/java/com/codestates/stackoverflow/dto/PageInfo.java
+++ server/src/main/java/com/codestates/stackoverflow/dto/PageInfo.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-
 @AllArgsConstructor
 @Getter
 @Setter
--- server/src/main/java/com/codestates/stackoverflow/questionComment/entity/QuestionComment.java
+++ server/src/main/java/com/codestates/stackoverflow/questionComment/entity/QuestionComment.java
@@ -7,8 +7,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
